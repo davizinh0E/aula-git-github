791543e1c056a8150eb3d99e6f07a34c3d6ec95a
Davi Eduardo <davi.eduardo059@gmail.com>
2024-09-21 11:15:18 -0300

    primeiro commit : app somatoria

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String args[]){
+        String entrada;
+        int numero1, numero2, soma;
+        entrada = JOptionPane.showInputDialog("Digite o número: ");
+       // Converte a entrada para inteiro e armazena em numero1
+        numero1 = Integer.parseInt(entrada);
+        entrada = JOptionPane.showInputDialog("Digite o número: ");
+        // Converte a entrada para inteiro e armazena em numero2
+       numero2 = Integer.parseInt(entrada);
+       soma = numero1 + numero2;
+       JOptionPane.showMessageDialog(null, "A soma é: " + soma);
+       System.exit(0);
+    }
+}
